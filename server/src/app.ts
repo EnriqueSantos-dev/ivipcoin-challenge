1cdcf42e57f049b3b74c951b071689321d78111d
Enrique Santos <santosenrique869@gmail.com>
2023-08-12 22:01:58 -0300

    feat: create app instance and add middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
+
+import { env } from '@/config/env'
 
 const app = express()
 
 app.use(express.json())
-app.use(cors({ origin: 'http://localhost' }))
+app.use(cors({ origin: ['http://localhost', env.clientUrl] }))
 
 export { app }
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,21 @@
-import { app } from "./app";
-import { env } from "./config/env";
+import 'express-async-errors'
+
+import { authRoutes, todosRoutes, usersRouter } from '@/routes'
+
+import { errorHandler } from '@/middlewares'
+
+import { initDatabase } from '@/config/database'
+import { env } from '@/config/env'
+
+import { app } from './app'
+
+app.use('/api/v1/todos', todosRoutes)
+app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/auth', authRoutes)
+app.use(errorHandler)
 
 app.listen(env.port, () => {
-	console.log(`Server is running on port ${env.port}`);
-});
-
-app.get("/hello", (req, res) => res.send("Hello World!"));
+	initDatabase().then(() => {
+		console.log(`Server is running on port ${env.port}`)
+	})
+})
