7faa1995cf3434e36dd8f913f7f9b21443599220
Enrique Santos <santosenrique869@gmail.com>
2023-08-12 22:02:30 -0300

    feat: create env config

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,7 +1,14 @@
-import * as dotEnv from "dotenv-safe";
+import * as dotEnv from 'dotenv-safe'
 
-dotEnv.config();
+dotEnv.config()
 
 export const env = {
-  port: process.env.PORT,
-};
+	port: process.env.PORT as string,
+	clientUrl: process.env.CLIENT_URL as string,
+	databaseName: process.env.DATABASE_NAME as string,
+	hashSalt: process.env.HASH_SALT as string,
+	jwt: {
+		expiresIn: process.env.JWT_EXPIRES_IN as string,
+		secret: process.env.JWT_SECRET as string,
+	},
+}
