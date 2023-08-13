72fe22ab05078fbd19f7ec06c988aa6c3d6c65aa
Enrique Santos <santosenrique869@gmail.com>
2023-08-12 22:09:20 -0300

    feat: create auth controller

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+import { AuthService } from '@/services'
+
+import { mapZodErrorsToFieldMessages } from '@/utils/zod-mapper'
+
+import { BadRequestError } from '@/helpers/http-error'
+
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	async login(request: Request, response: Response) {
+		const loginSchema = z.object({
+			email: z.string().min(1),
+			password: z.string().min(1),
+		})
+
+		const resultParsed = loginSchema.safeParse(request.body)
+
+		if (!resultParsed.success)
+			throw new BadRequestError(mapZodErrorsToFieldMessages(resultParsed.error))
+
+		const { email, password } = resultParsed.data
+		const token = await this.authService.login(email, password)
+
+		return response.status(200).json({ access_token: token })
+	}
+}
