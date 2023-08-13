18dd4e5fad517b4535ee1f31e7a1917a70538945
Enrique Santos <santosenrique869@gmail.com>
2023-08-12 22:04:55 -0300

    feat: create database config with AceBase

---
+++ server/src/config/database.ts
@@ -0,0 +1,25 @@
+import { AceBase } from 'acebase'
+
+import { env } from './env'
+
+export const database = new AceBase(env.databaseName as string, {
+	storage: {
+		path: './database',
+	},
+	logLevel: 'log',
+})
+
+export async function initDatabase() {
+	try {
+		await database.ready(async () => {
+			await Promise.all([
+				database.indexes.create('todos', 'userId'),
+				database.indexes.create('users', 'email'),
+				database.indexes.create('users', 'id'),
+			])
+		})
+	} catch (error) {
+		console.log(error)
+		throw new Error('Error creating database')
+	}
+}
