ee632b8d8c54dc95efbd62f86f73c4e44295a20a
Enrique Santos <santosenrique869@gmail.com>
2023-08-12 22:09:49 -0300

    feat: create users controller

---
+++ server/src/controllers/users.controller.ts
@@ -0,0 +1,34 @@
+import bcrypt from 'bcryptjs'
+import { Request, Response } from 'express'
+import { z } from 'zod'
+
+import { UsersService } from '@/services/users.service'
+
+import { mapZodErrorsToFieldMessages } from '@/utils/zod-mapper'
+
+import { BadRequestError } from '@/helpers/http-error'
+
+export class UsersController {
+	constructor(private readonly usersService: UsersService) {}
+
+	async createUser(request: Request, response: Response) {
+		const createdUserSchema = z.object({
+			name: z.string().min(3).max(255),
+			email: z.string().email(),
+			password: z.string().min(6).max(255),
+		})
+
+		const resultParsed = createdUserSchema.safeParse(request.body)
+
+		if (!resultParsed.success)
+			throw new BadRequestError(mapZodErrorsToFieldMessages(resultParsed.error))
+
+		const hashedPassword = await bcrypt.hash(resultParsed.data.password, 10)
+		const user = await this.usersService.createUser({
+			...resultParsed.data,
+			password: hashedPassword,
+		})
+
+		return response.status(201).json(user)
+	}
+}
