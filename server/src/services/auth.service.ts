aa908222e8b248cd34c39d6617ad6129bd7bb129
Enrique Santos <santosenrique869@gmail.com>
2023-08-14 21:57:32 -0300

    refactor: include user infos in response request on login

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,3 +1,5 @@
+import { UserMapper } from '@/entities/user/mappers/user.mapper'
+
 import { JwtService, UsersService } from '@/services'
 
 export class AuthService {
@@ -8,7 +10,14 @@ export class AuthService {
 
 	async login(email: string, password: string) {
 		const user = await this.usersService.validateUser(email, password)
+
 		const payload = { email: user.getEmail(), sub: user.id }
-		return this.jwtService.sign(payload)
+		const token = await this.jwtService.sign(payload)
+
+		return { token, user: UserMapper.toView(user) }
+	}
+
+	async getProfile(id: string) {
+		return this.usersService.getProfile(id)
 	}
 }
