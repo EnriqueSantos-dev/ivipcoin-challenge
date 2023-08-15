79f4ebcb343415f3277b80b9193e230680aa4c18
Enrique Santos <santosenrique869@gmail.com>
2023-08-14 22:04:39 -0300

    feat: add getProfile method

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -22,12 +22,18 @@ export class AuthController {
 			throw new BadRequestError(mapZodErrorsToFieldMessages(resultParsed.error))
 
 		const { email, password } = resultParsed.data
-		const token = await this.authService.login(email, password)
+		const { token, user } = await this.authService.login(email, password)
 
-		return response.status(200).json({ access_token: token })
+		return response.status(200).json({ user, access_token: token })
 	}
 
 	async verifyToken(request: Request, response: Response) {
 		return response.status(204).end()
+	}
+
+	async getProfile(request: Request, response: Response) {
+		const user = await this.authService.getProfile(request.user?.id as string) // id always exists because of the middleware
+
+		return response.status(200).json(user)
 	}
 }
